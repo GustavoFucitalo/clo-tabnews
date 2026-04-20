d5f4e90900f1941b81dc36e37b27b7eee25a8bf6
Gustavo fucitalo <fucitalo99@gmail.com>
2026-04-20 19:42:50 -0300

    adiciona limpar input

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,23 @@ import { useState } from "react";
 export default function Home() {
     const [favBand, setFavBand] = useState("")
 
+    const handlerClear = () => {
+        setFavBand('');
+        
+    }
+
     return (
         <div>
             <input 
                 type="text"
+                value={favBand}
                 placeholder="Type yout favorite band"
                 onChange={(e) => setFavBand(e.target.value)}
             />
             <h1>Your favorite band is {favBand}</h1>
+        <button onClick={handlerClear}>
+            Clear
+        </button>
         </div>
     );
 }
